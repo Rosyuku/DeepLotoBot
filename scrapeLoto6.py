b5444655287e49f9cc7848ba87b5c8c3278d6091
linasugi <sogenakotoga@hotmail.com>
2018-09-20 20:53:53 +0900

    最新の結果を取得してツイートする機能を追加

---
+++ scrapeLoto6.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 30 21:50:59 2017
+
+@author: Wakasugi Kazuyuki
+"""
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+def getlastNo():
+    url = "http://sougaku.com/loto6/data/detail/"
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    last = int(soup.find(class_='contents_ti6').find_all('h3')[0].text[4:8])
+    return last    
+    
+def getcolumnsList():
+    columnsList=[
+     '抽せん回',
+     '抽せん日',
+     '曜日',     
+     '販売実績額',
+     'セット球',
+     '本数字1',
+     '本数字2',
+     '本数字3',
+     '本数字4',
+     '本数字5',
+     '本数字6',
+     'ボーナス数字',
+     '1等口数',
+     '1等賞金',
+     '2等口数',
+     '2等賞金',
+     '3等口数',
+     '3等賞金',
+     '4等口数',
+     '4等賞金',
+     '5等口数',
+     '5等賞金',
+     'キャリーオーバー',
+     ]
+    return columnsList
+
+def getdataList(url="http://sougaku.com/loto6/data/detail/"):
+    dataList = []
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    tb1 = soup.find(class_='sokuho_tb1')
+    tb2 = soup.find(class_='sokuho_tb2')
+    tb3 = soup.find(class_='sokuho_tb3')    
+    for i in range(len(tb1.find_all('td'))):
+        dataList.append(tb1.find_all('td')[i].text.replace('\n', '').replace('\t', ''))    
+    for i in range(len(tb2.find_all('td'))):
+        dataList.append(tb2.find_all('td')[i].text.replace('\n', '').replace('\t', ''))
+    for i in range(len(tb3.find_all('td'))):
+        dataList.append(tb3.find_all('td')[i].text.replace('\n', '').replace('\t', ''))    
+    return pd.Series(dataList)[[0, 1, 1, 2, 3, 12, 13, 14, 15, 16, 17, 19, 21, 22, 25, 26, 29, 30, 33, 34, 37, 38, 41]]
+
+def getLogdata(getall=True, modify=True, last=-1):
+    
+    if last == -1:
+        last = getlastNo()
+    
+    columnsList = getcolumnsList()   
+
+    if getall == True: 
+        logdata = pd.DataFrame(index=range(1, last+1), columns=columnsList)
+    else:
+        logdata = pd.DataFrame(index=range(last, last+1), columns=columnsList)
+
+    for i in logdata.index:
+        
+        target = i
+        print(i)
+        
+        try:
+            url = "http://sougaku.com/loto6/data/detail/index" + str(target) + ".html"
+            logdata.loc[i, :] = getdataList(url).values
+        except:
+            url = "http://sougaku.com/loto6/data/detail/"
+            logdata.loc[i, :] = getdataList(url).values
+            
+#        if target == last:
+#            url = "http://sougaku.com/loto6/data/detail/"
+#        else:
+#            url = "http://sougaku.com/loto6/data/detail/index" + str(target) + ".html"
+#        
+#        logdata.loc[i, :] = getdataList(url).values
+
+    if modify == False:
+        return logdata
+    
+    pattern = '|'.join(["第", "回", "円", "口", ",", "該当なし"])
+
+    for i in range(logdata.columns.shape[0]):
+        
+        if i == 0 or i == 3 or i >= 5:
+            logdata.iloc[:, i] = pd.to_numeric(logdata.iloc[:, i].str.replace(pattern, "")).fillna(0).astype(int)
+        elif i == 1:
+            logdata.iloc[:, i] = logdata.iloc[:, i].str[:-3]
+        elif i == 2:
+            logdata.iloc[:, i] = logdata.iloc[:, i].str[-2]
+            
+    return logdata
+
+if __name__ == "__main__":
+    
+    logdata = getLogdata(False, False)
+
+    #logdata.to_csv('logdata.csv')
--- tweet.py
+++ tweet.py
@@ -7,12 +7,96 @@ Created on Thu Sep 20 18:05:49 2018
 
 import twitter
 import config
+import scrapeLoto6
 
-api = twitter.Api(consumer_key=config.consumer_key,
-                  consumer_secret=config.consumer_secret,
-                  access_token_key=config.access_token_key,
-                  access_token_secret=config.access_token_secret
-                  )
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+#import matplotlib.font_manager
+import numpy as np
+import cv2
+import os
 
-api.PostUpdate("test2")
+if __name__ == "__main__":
 
+    api = twitter.Api(consumer_key=config.consumer_key,
+                      consumer_secret=config.consumer_secret,
+                      access_token_key=config.access_token_key,
+                      access_token_secret=config.access_token_secret
+                      )
+    
+    logdata = scrapeLoto6.getLogdata(False, False, -1)
+    
+    
+    logdata2 = scrapeLoto6.getLogdata(False, True, -1)
+    logdata2['1等賞金総額'] = logdata2['1等口数'] * logdata2['1等賞金']
+    logdata2['2等賞金総額'] = logdata2['2等口数'] * logdata2['2等賞金']
+    logdata2['3等賞金総額'] = logdata2['3等口数'] * logdata2['3等賞金']
+    logdata2['4等賞金総額'] = logdata2['4等口数'] * logdata2['4等賞金']
+    logdata2['5等賞金総額'] = logdata2['5等口数'] * logdata2['5等賞金']
+    logdata2['ハズレ口数'] = (logdata2.iloc[:, 3] / 200).astype(int) - logdata2['1等口数'] - logdata2['2等口数'] - logdata2['3等口数'] - logdata2['4等口数'] - logdata2['5等口数']
+    #logdata2['ハズレ総額'] = logdata2['ハズレ口数'] * 200
+    logdata2['運営回収額'] = logdata2.iloc[:, 3] - logdata2['1等賞金総額'] - logdata2['2等賞金総額'] - logdata2['3等賞金総額'] - logdata2['4等賞金総額']- logdata2['5等賞金総額']
+    
+    logdata['販売口数'] = (logdata2.iloc[:, 3] / 200).astype(int).astype(str) + '口'
+    logdata['賞金総額'] = (logdata2['1等賞金総額'] + logdata2['2等賞金総額'] + logdata2['3等賞金総額'] + logdata2['4等賞金総額'] + logdata2['5等賞金総額']).astype(str) + '円'
+    logdata['期待値'] = ((logdata2['1等賞金総額'] + logdata2['2等賞金総額'] + logdata2['3等賞金総額'] + logdata2['4等賞金総額'] + logdata2['5等賞金総額']) / (logdata2.iloc[:, 3] / 200)).round().astype(str) + '円'
+
+    msg = "%sに実施された%sの結果は、本数字が%s,%s,%s,%s,%s,%s、ボーナス数字が%sでした。また、販売口数は%sで、賞金総額は%sですので、1口あたりの期待値は%sということになります。なお、次回キャリーオーバー額は%sとのことです。 #loto6 #ロト6" % tuple(logdata.iloc[0, [1, 0, 5, 6, 7, 8, 9, 10, 11,  -3, -2, -1, -4]])
+    #msg = "%sに実施された%sの結果は、本数字が%s,%s,%s,%s,%s,%s、ボーナス数字が%sとなったようです。なお、販売額としては%sで、次回キャリーオーバー額は%sとのことです。 #loto6 #ロト6" % tuple(logdata.iloc[0, [1, 0, 5, 6, 7, 8, 9, 10, 11, 3, -1]])
+    fig1path = 'media/figure_entries'+ str(logdata.index[0]).zfill(5) +'.png'
+    fig2path = 'media/figure_amount'+ str(logdata.index[0]).zfill(5) +'.png'
+    figpath = 'media/figure'+ str(logdata.index[0]).zfill(5) +'.png'
+
+    if os.path.exists(figpath) == False:
+    
+        ###データ###
+        data=logdata2.iloc[0, [12, 14, 16, 18, 20, -2]].values
+        label=['First','Second','Third','Forth','Fifth','Losing']
+        
+        ###綺麗に書くためのおまじない###
+        plt.style.use('ggplot')
+        plt.rcParams.update({'font.size':15})
+        
+        ###各種パラメータ###
+        size=(7,3.5) #凡例を配置する関係でsizeは横長にしておきます。
+        col=cm.Spectral(np.arange(len(data))/float(len(data))) #color指定はcolormapから好みのものを。
+        
+        ###pie###
+        plt.figure(figsize=size,dpi=100)
+        plt.title('Percentage (entries)')
+        plt.pie(data,colors=col,counterclock=False,startangle=90,autopct=lambda p:'{:.1f}%'.format(p) if p>=1.0 else '')
+        plt.subplots_adjust(left=0,right=0.7)
+        plt.legend(label,fancybox=True,loc='center left',bbox_to_anchor=(0.9,0.5))
+        plt.axis('equal') 
+        plt.savefig(fig1path,bbox_inches='tight',pad_inches=0.05)
+    
+        ###データ###
+        data=logdata2.iloc[0, [-7, -6, -5, -4, -3, -1]].values
+        label=['First','Second','Third','Forth','Fifth',"Host's"]
+        
+        ###綺麗に書くためのおまじない###
+        plt.style.use('ggplot')
+        plt.rcParams.update({'font.size':15})
+        
+        ###各種パラメータ###
+        size=(7,3.5) #凡例を配置する関係でsizeは横長にしておきます。
+        col=cm.Spectral(np.arange(len(data))/float(len(data))) #color指定はcolormapから好みのものを。
+        
+        ###pie###
+        plt.figure(figsize=size,dpi=100)
+        plt.title('Percentage (amount)')
+        plt.pie(data,colors=col,counterclock=False,startangle=90,autopct=lambda p:'{:.1f}%'.format(p) if p>=1.0 else '')
+        plt.subplots_adjust(left=0,right=0.7)
+        plt.legend(label,fancybox=True,loc='center left',bbox_to_anchor=(0.9,0.5))
+        plt.axis('equal') 
+        plt.savefig(fig2path,bbox_inches='tight',pad_inches=0.05)
+        
+        im1 = cv2.imread(fig1path)
+        im2 = cv2.imread(fig2path)
+        im_v = cv2.vconcat([im1, im2])
+        cv2.imwrite(figpath, im_v)
+        
+        api.PostUpdate(msg, media=figpath)
+        
+    else:
+        print("already exists")
